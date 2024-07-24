36f5ed933d97d43d89bb04bb38c0ae534e0a2d5a
Joshua W Julian <joshuawjulian@gmail.com>
2024-07-24 17:43:14 -0400

    messing with the table drawing

---
+++ packages/web/src/lib/components/TableWideView.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import type { GameStateType } from '@pkrdb/hand-engine';
+
+	let { state }: { state: GameStateType } = $props();
+</script>
+
+<section>
+	{#each state.players as player, i}
+		<div class="player">
+			<div class="name">{i}</div>
+			<div class="stack">{player.startingStack}</div>
+			<div class="hand">{player.cards}</div>
+		</div>
+	{/each}
+</section>
+
+<style>
+	section {
+		width: 100%;
+		display: flex;
+		gap: 2rem;
+		border: 1rem solid var(--brand);
+	}
+
+	.player {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		border: 1px solid var(--border-color);
+		border-radius: 0.5rem;
+		padding: 1rem;
+	}
+</style>
